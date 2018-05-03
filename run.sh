05ee09c581f7039c7a12369eaca37876632faa89
Michelle Oliveira <michelleolivcosta@gmail.com>
2018-05-03 18:17:26 -0300

    Update run.sh
    colocando porta https

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker build -t seg/security-messenger .
 
 docker run -p 5433:5432 -d --env POSTGRES_USER=postgres --env POSTGRES_DB=security-messenger --env POSTGRES_PASSWORD=postgres --name securitydb postgres
 docker run -p 27017:27017 -d --name mongosec mongo
-docker run -d -p 8080:8080 -p 4848:4848 --link securitydb:host-pg --link mongosec:host-mongo --name security-messenger seg/security-messenger 
+docker run -d -p 8080:8181 -p 4848:4848 --link securitydb:host-pg --link mongosec:host-mongo --name security-messenger seg/security-messenger 
